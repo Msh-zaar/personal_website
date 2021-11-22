731b9070742abbcb4fb412db79e5bf66d52fc310
msh-zaar <shareabletie@DESKTOP-8LK9FE7.localdomain>
2021-11-22 19:40:57 +0100

    new colour scheme

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -9,36 +9,35 @@ const Contact = () => {
             <div className="c-bg">
                 <div className="c-bg2"></div>
             </div>
-                
-                    <div className="c-wrapper">
-                        <div className="c-right">
-                            <h1 className="c-title">Contact Me</h1>
-                                <div className="c-info">
-                                    <div className="c-info-item">
-                                        <img 
-                                        src={Phone}
-                                        alt="" 
-                                        className="c-icon" 
-                                        />
-                                        +47 984 11 506
-                                    </div>
-                                    <div className="c-info-item">
-                                        <img 
-                                        src={Mail}
-                                        alt="" 
-                                        className="c-icon" 
-                                        />
-                                        michael.sh.zaar@gmail.com
-                                    </div>
-                                    <div className="c-info-item">
-                                        <img 
-                                        src={Location}
-                                        alt="" 
-                                        className="c-icon" 
-                                        />
-                                        0772, Oslo
-                                    </div>
+                <div className="c-wrapper">
+                    <div className="c-right">
+                        <h1 className="c-title">Contact Me</h1>
+                            <div className="c-info">
+                                <div className="c-info-item">
+                                    <img 
+                                    src={Phone}
+                                    alt="" 
+                                    className="c-icon" 
+                                    />
+                                    +47 984 11 506
                                 </div>
+                                <div className="c-info-item">
+                                    <img 
+                                    src={Mail}
+                                    alt="" 
+                                    className="c-icon" 
+                                    />
+                                    michael.sh.zaar@gmail.com
+                                </div>
+                                <div className="c-info-item">
+                                    <img 
+                                    src={Location}
+                                    alt="" 
+                                    className="c-icon" 
+                                    />
+                                    0772, Oslo
+                                </div>
+                            </div>
                         </div>
             </div>
         </div>
